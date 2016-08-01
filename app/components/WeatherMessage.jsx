ec7fc2db42fbfb92a9020c0f772d9ec7e6714375
Jackson Tong <tongtoan2704@gmail.com>
2016-08-01 19:03:37 +0700

    Weather form

---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const WeatherMessage = React.createClass({
+  render() {
+    return (
+      <div>
+        <p>Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Praesent sapien massa, convallis a
+          pellentesque nec, egestas non nisi. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Quisque
+          velit nisi, pretium ut lacinia in, elementum id enim. Praesent sapien massa, convallis a pellentesque nec,
+          egestas non nisi. Nulla porttitor accumsan tincidunt. Quisque velit nisi, pretium ut lacinia in, elementum id
+          enim. Cras ultricies ligula sed magna dictum porta. Curabitur non nulla sit amet nisl tempus convallis quis ac
+          lectus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.</p>
+      </div>
+    )
+  }
+});
+
+export default WeatherMessage;
