173b2b3e9a32b2f16c731bacb7d1ce1b0dccafde
Jackson Tong <tongtoan2704@gmail.com>
2016-08-01 17:45:20 +0700

    Add active link

--- app/app.jsx
+++ app/app.jsx
@@ -4,11 +4,13 @@ import {Route, Router, IndexRoute, hashHistory} from 'react-router';
 import Main from './components/Main.jsx';
 import Weather from './components/Weather.jsx';
 import About from './components/About.jsx';
+import Examples from './components/Examples.jsx';
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
       <Route path="about" component={About}/>
+      <Route path="examples" component={Examples}/>
       <IndexRoute component={Weather}/>
     </Route>
   </Router>, 
