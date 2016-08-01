e66828d48349c29a4dc554fb79ea8739fc5d9088
Jackson Tong <tongtoan2704@gmail.com>
2016-08-01 17:34:58 +0700

    Nav and Weather components

--- app/app.jsx
+++ app/app.jsx
@@ -1,4 +1,16 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Route, Router, IndexRoute, hashHistory} from 'react-router';
+import Main from './components/Main.jsx';
+import Weather from './components/Weather.jsx';
+import About from './components/About.jsx';
 
-ReactDOM.render(<h1>Boilerplate app!</h1>, document.getElementById('app'));
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+      <Route path="about" component={About}/>
+      <IndexRoute component={Weather}/>
+    </Route>
+  </Router>, 
+  document.getElementById('app')
+);
