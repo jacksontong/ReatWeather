a5ef0ede6588ee3dad6e1d448a342de528ea89f4
Jackson Tong <tongtoan2704@gmail.com>
2016-08-02 22:27:23 +0700

    add foundation & jQuery (cherry picked from commit b8ae7c8)

--- app/app.jsx
+++ app/app.jsx
@@ -1,4 +1,12 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Route, Router, IndexRoute, hashHistory} from 'react-router';
+import Main from './components/Main.jsx';
+import Weather from './components/Weather.jsx';
+import About from './components/About.jsx';
+import Examples from './components/Examples.jsx';
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
 
 ReactDOM.render(<h1>Boilerplate app!</h1>, document.getElementById('app'));
