b8ae7c8a59863dfd2fa4dd43c7603f849b8db68f
Jackson Tong <tongtoan2704@gmail.com>
2016-08-02 22:27:23 +0700

    add foundation & jQuery

--- app/app.jsx
+++ app/app.jsx
@@ -5,6 +5,9 @@ import Main from './components/Main.jsx';
 import Weather from './components/Weather.jsx';
 import About from './components/About.jsx';
 import Examples from './components/Examples.jsx';
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
 
 ReactDOM.render(
   <Router history={hashHistory}>
