bb97f72fd4ef91d1eff2a053976e52fab5b0284f
Jackson Tong <tongtoan2704@gmail.com>
2016-08-02 21:04:43 +0700

    update example and about content

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const About = (props) => {
   return (
     <div>
-      <h3>About Component</h3>
+      <h3>About</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur eius fugiat modi molestiae officiis
+        quidem quo! Alias corporis delectus distinctio enim facere ipsa minima non, numquam quas sit tempore
+        voluptatum.</p>
     </div>
   );
 };
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Examples = (props) => {
   return (
     <div>
-      <h3>Examples Component</h3>
+      <h3>Examples</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus fugit incidunt maiores molestias nam
+        ratione tenetur. Deleniti, pariatur, suscipit! Adipisci architecto autem dicta eligendi exercitationem labore
+        mollitia officiis provident ullam.</p>
     </div>
   );
 };
