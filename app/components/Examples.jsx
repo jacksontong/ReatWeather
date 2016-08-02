9f09c8d3374535a9874697973c676784e44267db
Jackson Tong <tongtoan2704@gmail.com>
2016-08-02 22:51:33 +0700

    Add grid system & style examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 const Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
+      <h1 className="text-center">Examples</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus fugit incidunt maiores molestias nam
         ratione tenetur. Deleniti, pariatur, suscipit! Adipisci architecto autem dicta eligendi exercitationem labore
         mollitia officiis provident ullam.</p>
+      <ol>
+        <li>
+          <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
     </div>
   );
 };
