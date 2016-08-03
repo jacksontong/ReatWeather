bd1fe0e9836b1b49945f71ea104e7af92754465d
Jackson Tong <tongtoan2704@gmail.com>
2016-08-03 23:23:54 +0700

    update boilerplate

--- app/app.jsx
+++ app/app.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Route, Router, IndexRoute, hashHistory} from 'react-router';
 import Main from './components/Main.jsx';
-import Weather from './components/Weather.jsx';
-import About from './components/About.jsx';
-import Examples from './components/Examples.jsx';
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
-ReactDOM.render(<h1>Boilerplate app!</h1>, document.getElementById('app'));
+//App css
+require('style!css!sass!./styles/app.scss');
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path='/' component={Main}>
+    </Route>
+  </Router>
+  , document.getElementById('app')
+);
